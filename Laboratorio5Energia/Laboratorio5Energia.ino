1182501b69781c25b9fdcd9ea38e31dc355b693c
men18300 <77988142+men18300@users.noreply.github.com>
2021-04-04 22:40:23 -0600

    Errores corregidos del menu

--- Laboratorio5Energia/Laboratorio5Energia.ino
+++ Laboratorio5Energia/Laboratorio5Energia.ino
@@ -1,6 +1,15 @@
+//Universidad del Valle de Guatemala
+//Departamento de Ingenieria Mecatronica y Electronica
+//Digital 2
+//Diego Mencos
+//Carne 18300
+
+//Incluimos la librerias necesarias
 #include <SPI.h>
 #include <SD.h>
 
+
+//Nuestra variable para la entrada del usuario
 int seleccion;
 
 File myFile;
@@ -24,53 +33,45 @@ void setup()
     return;
   }
   Serial.println("initialization done.");
-
-
-
-
-
   myFile = SD.open("/");
-
   printDirectory(myFile, 0);
-
   Serial.println("done!");
 
+  menu();
+
+
 }
+
 
 void loop()
 {
 
-
   if (Serial.available() > 0) {
 
-    Serial.println("Selecciona la imagen que deseas ver:");
-    Serial.println("1---->Para ver a Donkey Kong:");
-    Serial.println("1---->Para ver a Link:");
-    Serial.println("1---->Para ver a Scorpion:");
     seleccion = Serial.read();
+
     if (seleccion == '1') {
       Serial.println("Este esss Donkey Kong:");
       imprimirDonkey();
-      
+
     }
-    else if (seleccion == 2) {
+    else if (seleccion == '2') {
       imprimirLink();
-      
+
     }
-    else if (seleccion == 3) {
+    else if (seleccion == '3') {
       imprimirScorpion();
-      
+
     }
     else {
       Serial.println("Lo siento :( esa opcion no existe");
+
     }
-
   }
-
-
-
 }
 
+
+//Funcion para imprimir a Link (Zelda)
 void imprimirLink(void) {
   myFile = SD.open("LINK8B~1.txt");
   if (myFile) {
@@ -86,9 +87,11 @@ void imprimirLink(void) {
     // if the file didn't open, print an error:
     Serial.println("error opening test.txt");
   }
-
+  menu();
 }
 
+
+//Funcion para imprimir a Scorpion de Mortal Kombat
 void imprimirScorpion(void) {
   myFile = SD.open("SCORPI~1.txt");
   if (myFile) {
@@ -104,8 +107,11 @@ void imprimirScorpion(void) {
     // if the file didn't open, print an error:
     Serial.println("error opening test.txt");
   }
+  menu();
 }
 
+
+//Funcion para imprimir el Donkey Kong
 void imprimirDonkey(void) {
   myFile = SD.open("DONKEY~1.txt");
   if (myFile) {
@@ -121,10 +127,11 @@ void imprimirDonkey(void) {
     // if the file didn't open, print an error:
     Serial.println("error opening test.txt");
   }
+  menu();
 }
 
 
-
+//Funcion que nos muestra el directorio de la SD
 void printDirectory(File dir, int numTabs) {
   while (true) {
 
@@ -146,5 +153,17 @@ void printDirectory(File dir, int numTabs) {
       Serial.println(entry.size(), DEC);
     }
     entry.close();
+
+
   }
+}
+
+void menu(void) {
+  Serial.println("");
+  Serial.println("");
+  Serial.println("Selecciona la imagen que deseas ver:");
+  Serial.println("1---->Para ver a Donkey Kong:");
+  Serial.println("2---->Para ver a Link:");
+  Serial.println("3---->Para ver a Scorpion:");
+
 }
